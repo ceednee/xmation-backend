8f252c0d0fd4090710f7f4f3b657e19d00dbf208
Ceednee <ceednee@gmail.com>
2026-03-09 11:27:56 +0100

    chore(db): remove auth HTTP routes from Convex
    - Remove auth.addHttpRoutes(http) from http.ts
    - Clean up unused auth import
    
    Convex HTTP router no longer handles auth endpoints

--- convex/http.ts
+++ convex/http.ts
@@ -1,9 +1,5 @@
 import { httpRouter } from "convex/server";
-import { auth } from "./auth";
 
 const http = httpRouter();
-
-// Mount Convex Auth at /auth
-auth.addHttpRoutes(http);
 
 export default http;
